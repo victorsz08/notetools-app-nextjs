f5b26b6ef1c3828f5f08a6846b3cfdd71e12af90
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2025-03-10 22:56:20 +0000

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Poppins, Afacad } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--poppins"
+});
+
+const afacad = Afacad({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--afacad"
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${poppins.variable} ${afacad.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
