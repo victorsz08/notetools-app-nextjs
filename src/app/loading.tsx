1663a278c4aefcae0702dae7fbfcc979007d8c41
victorsz08 <joaovictorsal777@gmail.com>
2025-03-17 11:06:53 -0300

    Feature: Add requisições de current orders; Fix: Correção nos headers das requisições axios

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,10 +5,12 @@
 
 export default function Loading() {
     return (
-        <div className="flex flex-row gap-2">
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce" />
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.3s]" />
-        <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.5s]" />
-      </div>
+        <section>
+            <div className="flex flex-row gap-2">
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce" />
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.3s]" />
+            <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.5s]" />
+            </div>
+        </section>
     )
 }
